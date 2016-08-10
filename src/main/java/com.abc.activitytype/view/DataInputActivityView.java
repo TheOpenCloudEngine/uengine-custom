c806786eb83ee5e484cc3e183ad18d7f7503fd87
admin <admin>
2016-08-10 16:56:14 +0900

    no message

---
+++ src/main/java/com.abc.activitytype/view/DataInputActivityView.java
@@ -0,0 +1,9 @@
+package com.abc.activitytype.view;
+
+import org.uengine.kernel.view.ActivityView;
+
+/**
+ * Created by jjy on 2016. 8. 8..
+ */
+public class DataInputActivityView extends ActivityView {
+}
