09f3aade80be924bc4069e064b5bcd517f29e01a
basquiat <funnyjazz@naver.com>
2016-08-11 01:31:39 +0900

    merge 깨짐 수정

--- src/main/java/com.abc.activitytype/view/DataInputActivityView.java
+++ src/main/java/com.abc.activitytype/view/DataInputActivityView.java
@@ -3,11 +3,7 @@ package com.abc.activitytype.view;
 import org.uengine.kernel.view.ActivityView;
 
 /**
-<<<<<<< HEAD
- * Created by jjy on 2016. 8. 8..
-=======
  * Created by jjy on 2016-08-08.
->>>>>>> 8b834f34eb5d1f5fe843cdd899d68dd6caa20557
  */
 public class DataInputActivityView extends ActivityView {
 }
--- src/main/java/com.abc.activitytype/view/TableSelector.java
+++ src/main/java/com.abc.activitytype/view/TableSelector.java
@@ -4,16 +4,6 @@ import org.metaworks.Face;
 import org.metaworks.component.SelectBox;
 
 /**
-<<<<<<< HEAD
- * Created by jjy on 2016. 8. 8..
- */
-public class TableSelector extends SelectBox implements Face<String> {
-
-    public TableSelector(){
-
-        super();
-
-=======
  * Created by jjy on 2016-08-08.
  */
 public class TableSelector extends SelectBox implements Face<String> {
@@ -32,21 +22,13 @@ public class TableSelector extends SelectBox implements Face<String> {
     public TableSelector(){
 
         super();
->>>>>>> 8b834f34eb5d1f5fe843cdd899d68dd6caa20557
         getOptionNames().add("Table1");
         getOptionNames().add("Table2");
         getOptionNames().add("Table3");
         getOptionNames().add("Table4");
-<<<<<<< HEAD
-
-    }
-
-
-=======
         setOptionValues(getOptionNames());
     }
 
->>>>>>> 8b834f34eb5d1f5fe843cdd899d68dd6caa20557
     @Override
     public void setValueToFace(String value) {
         setSelected(value);
@@ -56,8 +38,4 @@ public class TableSelector extends SelectBox implements Face<String> {
     public String createValueFromFace() {
         return getSelected();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 8b834f34eb5d1f5fe843cdd899d68dd6caa20557
