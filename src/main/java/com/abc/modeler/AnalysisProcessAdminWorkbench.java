b14f354fc80912733df5f4801cd7be922f7282bc
SeungpilPark <sppark@uengine.org>
2016-08-18 09:30:33 +0900

    Data resource recover

--- src/main/java/com/abc/modeler/AnalysisProcessAdminWorkbench.java
+++ src/main/java/com/abc/modeler/AnalysisProcessAdminWorkbench.java
@@ -1,19 +1,33 @@
 package com.abc.modeler;
 
-import org.uengine.processadmin.ProcessAdminWorkbench;
+import com.abc.modeler.dbnavigator.DatabaseNavigator;
+import org.uengine.modeling.resource.Workbench;
+import org.uengine.processadmin.ProcessAdminResourceNavigator;
+import org.uengine.processadmin.RecentEditedResourcesPanel;
 
 /**
  * Created by jjy on 2016. 8. 12..
  */
-public class AnalysisProcessAdminWorkbench extends ProcessAdminWorkbench {
+public class AnalysisProcessAdminWorkbench extends Workbench {
 
 
-    public AnalysisProcessAdminWorkbench(){
+    public AnalysisProcessAdminWorkbench() {
 
-        super();
+        super(new ProcessAdminResourceNavigator());
 
-        setResourceNavigator(new AnalysisResourceNavigator());
+        try {
+            setEditorPanel(new RecentEditedResourcesPanel(getResourceNavigator()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    DatabaseNavigator databaseNavigator;
+    public DatabaseNavigator getDatabaseNavigator() {
+        return new DatabaseNavigator();
+    }
+    public void setDatabaseNavigator(DatabaseNavigator databaseNavigator) {
+        this.databaseNavigator = databaseNavigator;
     }
 
 
