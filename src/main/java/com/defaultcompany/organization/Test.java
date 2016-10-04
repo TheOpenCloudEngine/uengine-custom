86096a6c062df37fa53421d6f2e29fa90e1d5db2
basquiat <funnyjazz@naver.com>
2016-10-04 09:20:08 +0900

    processModeler example

---
+++ src/main/java/com/defaultcompany/organization/Test.java
@@ -0,0 +1,17 @@
+package com.defaultcompany.organization;
+
+import org.uengine.processmanager.ProcessManagerRemote;
+
+/**
+ * Created by Administrator on 2016-09-29.
+ */
+public class Test {
+
+    public static void main(String [] args) throws Exception{
+
+        ProcessManagerRemote prm;
+
+
+
+    }
+}
