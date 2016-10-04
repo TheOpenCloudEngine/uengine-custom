7030718acd8d031d8b6010d26d6d7ecfbf72ee69
basquiat <funnyjazz@naver.com>
2016-10-04 19:23:59 +0900

    RESTful example

--- src/main/java/com/abc/controller/TestController.java
+++ src/main/java/com/abc/controller/TestController.java
@@ -2,70 +2,36 @@ package com.abc.controller;
 
 import com.abc.service.ProcessService;
 import com.google.gson.Gson;
-import org.oce.garuda.multitenancy.TenantContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.uengine.kernel.ProcessInstance;
-import org.uengine.processmanager.ProcessManagerRemote;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Scope("request")
 public class TestController {
 
 	@Autowired
-	@Qualifier("processManagerBeanForQueue")
-	ProcessManagerRemote pm;
+	ProcessService processService;
 
-	@RequestMapping(value = "test.do", method = RequestMethod.GET)
-	public String callJsp() {
-		return "test";
-	}
-
-	@RequestMapping(value = "call.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/callProcess", method = RequestMethod.GET)
 	public String callTestPage() {
 		return "callProcess";
 	}
 
-	@RequestMapping(value = "initProcess.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/runProcess", method = RequestMethod.POST)
 	@ResponseBody
-	@Transactional
-	public String initProcess(@RequestParam Map<String, String> paramMap) throws Exception{
+	public String runProcess(@RequestParam Map<String, String> paramMap) throws Exception{
 
-		new TenantContext("1");
-
-		String defId = paramMap.get("defId");
-		String userId = paramMap.get("userId");
-
-		//ProcessExecutionThread processExecutionThread = MetaworksRemoteService.getComponent(ProcessExecutionThread.class);
-		String initInstanceId = "";
-		ProcessInstance instance = null;
-		try {
-			initInstanceId = pm.initializeProcess(defId);
-			instance = pm.getProcessInstance(initInstanceId);
-
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-
-		if(instance == null){
-			try {
-				throw new Exception("Porcess is not initialized.");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
+		processService.runProcess(paramMap);
 		Map<String, String> returnMap = new HashMap<String, String>();
-		returnMap.put("instanceId", initInstanceId);
-
+		returnMap.put("result", "result6");
 		String json = new Gson().toJson(returnMap);
 		return json;
 	}
--- src/main/java/com/abc/service/ProcessService.java
+++ src/main/java/com/abc/service/ProcessService.java
@@ -1,46 +1,75 @@
 package com.abc.service;
 
 
+import org.metaworks.dwr.MetaworksRemoteService;
+import org.oce.garuda.multitenancy.TenantContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.uengine.codi.mw3.model.IInstance;
+import org.uengine.codi.mw3.model.Instance;
+import org.uengine.codi.mw3.model.WorkItemHandler;
+import org.uengine.kernel.EJBProcessInstance;
 import org.uengine.kernel.ProcessInstance;
+import org.uengine.kernel.RoleMapping;
 import org.uengine.processmanager.ProcessManagerRemote;
+
+import java.rmi.RemoteException;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016-09-29.
  */
-@Service("processService")
+@Component("processService")
+@Scope("request")
 public class ProcessService {
 
     @Autowired
     @Qualifier("processManagerBeanForQueue")
-    ProcessManagerRemote pm;
+    ProcessManagerRemote pm;  //@Service 로 ProcessService를 선언할 시에는 이 것이 재사용되어 문제가 발생한다.
 
     @Transactional
-    public String runProcess(String defId, String userId){
+    public void runProcess(Map<String, String> paramMap) throws Exception{
+//        this.run(paramMap);
+//    }
+//
+//    public void run(Map<String, String> paramMap) throws Exception{
+        //ProcessManagerRemote pm = MetaworksRemoteService.getComponent(ProcessManagerRemote.class);
+        //tenant setting
+        String defId = paramMap.get("defId");
+        String userId = paramMap.get("userId");
+        String userName = paramMap.get("userName");
+        new TenantContext(paramMap.get("tenantId"));
 
-        //ProcessExecutionThread processExecutionThread = MetaworksRemoteService.getComponent(ProcessExecutionThread.class);
         String initInstId = "";
         ProcessInstance instance = null;
         try {
             initInstId = pm.initializeProcess(defId);
             instance = pm.getProcessInstance(initInstId);
+            if("1".equals(paramMap.get("isSim"))) {
+                ((EJBProcessInstance)instance).getProcessInstanceDAO().set("isSim", Integer.valueOf(1));
+                instance.setName("[Test] "+instance.getName());
+            }
 
+            RoleMapping roleMapping = RoleMapping.create();
+            roleMapping.setName("Initiator");
+            roleMapping.setEndpoint(userId);
+            roleMapping.setResourceName(userName);
+            roleMapping.fill(instance);
+            instance.putRoleMapping(roleMapping);
+
+            EJBProcessInstance ejbProcessInstance = (EJBProcessInstance)instance;
+            ejbProcessInstance.getProcessInstanceDAO().set("initEP", "1");
+
+            pm.putRoleMapping(initInstId, roleMapping);
+            pm.setLoggedRoleMapping(roleMapping);
+            pm.executeProcess(initInstId);
+            pm.applyChanges();
         }catch (Exception e){
             e.printStackTrace();
         }
-
-        if(instance == null){
-            try {
-                throw new Exception("Porcess is not initialized.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return initInstId;
     }
-
 }
